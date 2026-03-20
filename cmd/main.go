cafab4162b2f7029450727647af334c055494288
JamesK <james.kavazy@gmail.com>
2026-03-20 19:51:24 +0000

    refactor: Scan hands into scanner buffer rather than reading whole file at once into memory

--- cmd/main.go
+++ cmd/main.go
@@ -10,10 +10,11 @@ func main() {
 	// fileSystem := os.DirFS("C:\\Users\\james\\testfolder")
 	fileSystem := os.DirFS("C:\\Users\\james\\AppData\\Local\\PokerStars.UK\\HandHistory\\KavarzE")
 	hands, err := pokerhud.HandHistoryFromFS(fileSystem)
-	fmt.Println(hands[0])
+
+	fmt.Printf("%#v\n", hands[0])
 	fmt.Println(len(hands))
 	fmt.Println("Errors: ", len(err))
 	for _, e := range err {
-		fmt.Printf(e.Error(), "\n\n")
+		fmt.Printf("%#v\n\n", e.Error())
 	}
 }
