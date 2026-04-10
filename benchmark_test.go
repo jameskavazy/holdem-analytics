e56b26db171cc68c8180f52d62d0b4f36c14d26f
JamesK <james.kavazy@gmail.com>
2026-04-10 13:34:27 +0100

    test: Clean up benchmark on real fs

--- benchmark_test.go
+++ benchmark_test.go
@@ -8,12 +8,8 @@ import (
 
 func BenchmarkHandHistoryFromFS(b *testing.B) {
 	fileSystem := os.DirFS("C:\\Users\\james\\testfolder")
+
 	for i := 0; i < b.N; i++ {
 		hands.ExportHands(fileSystem)
-		// hands.ExportHands(fstest.MapFS{
-		// 	"zoom.txt":      {Data: []byte(zoomHand1)},
-		// 	"cash game.txt": {Data: []byte(cashGame1)},
-		// 	"failure.txt":   {Data: []byte("not a hand")},
-		// })
 	}
 }
